11d97584175f9c3bad792f75befe211718419290
kauarsantoss <kaua.santos@germinare.org.br>
2024-09-21 13:20:06 -0300

    Construindo repositorio de Usuario

---
+++ src/main/java/org/example/construconectaapisql/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package org.example.construconectaapisql.repository;
+
+import org.example.construconectaapisql.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findById(Long usuarioId);
+    List<Usuario> findByUuid(String uuid);
+    List<Usuario> findByNomeCompletoLikeIgnoreCase(String nomeCompleto);
+    List<Usuario> findByNomeUsuarioLikeIgnoreCase(String nomeUsuario);
+    List<Usuario> findByCpf(String cpf);
+    List<Usuario> findByEmailLikeIgnoreCase(String email);
+    List<Usuario> findByTelefone(String telefone);
+    List<Usuario> findByGenero(Integer genero);
+}
