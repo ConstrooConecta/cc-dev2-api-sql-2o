fc78a52f2e20a2efe688c9c82e18f86fef126fee
Roger13san <roger.santana@germinare.org.br>
2024-10-19 19:07:01 -0300

    Criação de filtro de categoria por nome e ID

--- src/main/java/org/example/construconectaapisql/repository/ProdutoRepository.java
+++ src/main/java/org/example/construconectaapisql/repository/ProdutoRepository.java
@@ -1,10 +1,12 @@
 package org.example.construconectaapisql.repository;
 
+import org.example.construconectaapisql.model.Categoria;
 import org.example.construconectaapisql.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     Optional<Produto> findById(Long produtoId);
@@ -12,4 +14,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     List<Produto> findByCondicao(Boolean condicao);
     List<Produto> findByUsuario(String usuario);
     List<Produto> findByTopico(Integer topico);
+    List<Produto> findByCategorias(Set<Categoria> categorias);
+
+    List<Produto> findByNomeCategoria(String nome);
 }
